ede6e5d205d9574c8c10c1a789f30f565622e3a0
NatanRei <nreischmura@gmail.com>
2026-02-03 20:20:08 -0300

    feat: initiate db connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,19 @@
 import fastify from "fastify";
+import { prisma } from "./lib/prisma";
 
 export const app = fastify();
 
 app.get("/", async () => {
   return { message: "Hello World!" };
+});
+
+app.post("/users", async () => {
+  const res = await prisma.user.create({
+    data: {
+      name: "Natan",
+      email: "natan@teste.com",
+      passwordHash: "123456"
+    }
+  })
+  return { message: "User created successfully!", user: res };
 });
---
+++ src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import { env } from "@/env";
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
+  url: env.DATABASE_URL,
+});
+
+export const prisma = new PrismaClient({
+  log: env.NODE_ENV === "dev" ? ["query"] : [],
+  adapter,
+});
