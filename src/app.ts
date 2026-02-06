18a4b90a08d0906ac524fec79193a3db864b36e2
NatanRei <nreischmura@gmail.com>
2026-02-05 22:21:43 -0300

    feat: create and find user

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
-import fastify from "fastify";
+import { hash } from "bcryptjs";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import z from "zod";
 import { prisma } from "./lib/prisma";
 
 export const app = fastify();
@@ -7,13 +9,45 @@ app.get("/", async () => {
   return { message: "Hello World!" };
 });
 
-app.post("/users", async () => {
-  const res = await prisma.user.create({
+app.post("/users", async (req: FastifyRequest, res: FastifyReply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+    password: z.string().min(6),
+  });
+
+  const { name, email, password } = registerBodySchema.parse(req.body);
+
+  const passwordHash = await hash(password, 6);
+
+  const user = await prisma.user.create({
     data: {
-      name: "Natan",
-      email: "natan@teste.com",
-      passwordHash: "123456"
-    }
-  })
-  return { message: "User created successfully!", user: res };
+      name,
+      email,
+      passwordHash,
+    },
+  });
+
+  return res.status(201).send({ id: user.id });
+});
+
+app.get("/users/:id", async (req: FastifyRequest, res: FastifyReply) => {
+  const getUserSchema = z.object({
+    id: z.uuid(),
+  });
+
+  const { id } = getUserSchema.parse(req.params);
+
+  const user = await prisma.user.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  return res.send(user);
 });
