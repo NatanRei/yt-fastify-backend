4b66bddf5537d05c8bad0f0fda23fb3a1635d28d
NatanRei <nreischmura@gmail.com>
2026-02-07 14:00:03 -0300

    feat: add pagination, update, and deletion

--- src/app.ts
+++ src/app.ts
@@ -51,3 +51,83 @@ app.get("/users/:id", async (req: FastifyRequest, res: FastifyReply) => {
 
   return res.send(user);
 });
+
+app.get("/users", async (req: FastifyRequest, res: FastifyReply) => {
+  const getUsersSchema = z.object({
+    page: z.coerce.number().default(1),
+    perPage: z.coerce.number().default(10),
+    name: z.string().optional(),
+    email: z.string().optional(),
+  });
+
+  const { name, email, page, perPage } = getUsersSchema.parse(req.query);
+
+  const where = {
+    ...(name ? { name: { contains: name } } : {}),
+    ...(email ? { email: { contains: email } } : {}),
+  };
+
+  const data = await prisma.user.findMany({
+    where,
+    skip: (page - 1) * perPage,
+    take: perPage,
+    select: {
+      id: true,
+      name: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  const total = await prisma.user.count({ where });
+
+  return res.send({
+    data,
+    meta: {
+      total,
+      page,
+      perPage,
+      totalPages: Math.ceil(total / perPage),
+    },
+  });
+});
+
+app.put("/users/:id", async (req: FastifyRequest, res: FastifyReply) => {
+  const updateUserIdSchema = z.object({
+    id: z.uuid(),
+  });
+
+  const { id } = updateUserIdSchema.parse(req.params);
+
+  const updateBodySchema = z.object({
+    name: z.string(),
+    email: z.email(),
+  });
+
+  const { name, email } = updateBodySchema.parse(req.body);
+
+  await prisma.user.update({
+    where: { id },
+    data: {
+      name,
+      email,
+    },
+  });
+
+  return res.status(204).send();
+});
+
+app.delete("/users/:id", async (req: FastifyRequest, res: FastifyReply) => {
+  const deleteUserSchema = z.object({
+    id: z.uuid(),
+  });
+
+  const { id } = deleteUserSchema.parse(req.params);
+
+  await prisma.user.delete({
+    where: { id },
+  });
+
+  return res.status(204).send();
+});
